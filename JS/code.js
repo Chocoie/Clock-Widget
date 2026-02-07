013369088419f34313ebfd4d3b9a25c99f7e98ec
Chocoie <87912667+Chocoie@users.noreply.github.com>
2026-02-07 15:32:53 -0500

    added animation

--- JS/code.js
+++ JS/code.js
@@ -1,8 +1,42 @@
 let hrs = document.getElementById("hrs");
 let mins = document.getElementById("mins");
-let currTime = new Date();
+let snoopy = document.getElementById("snoopyImg");
+let lastHour = new Date().getHours();
+let lastMin = new Date().getMinutes();
 
 setInterval(() => {
+    let currTime = new Date();
     hrs.innerHTML = ((currTime.getHours()%12)<10?"0":"") + (currTime.getHours()%12);
-    mins.innerHTML = (currTime.getMinutes()<10?"0":"") + currTime.getMinutes();
+    mins.innerHTML = (currTime.getMinutes()<10?"0":"") + currTime.getMinutes();  
+
+    // TEMPORARY TEST: Triggers every second
+    if (lastMin !== currTime.getMinutes()) {
+        doAnimation();
+        lastMin = currTime.getMinutes();
+    }
+
+    if(lastHour !== currTime.getHours())
+    {
+        doAnimation();
+        lastHour = currTime.getHours();
+    }
 }, 1000);
+
+function doAnimation () 
+{
+    const clock = document.getElementsByClassName("clockSpan");
+    
+    for(let span of clock)
+    {
+        span.classList.add("hourBump");
+    }
+    snoopy.classList.add("activateSnoopy");
+
+    setTimeout(() => {
+        for(let span of clock)
+        {
+            span.classList.remove("hourBump");
+        }
+        snoopy.classList.remove("activateSnoopy");
+    }, 800);
+}
